994c273e076080c01fc32d9ddb7d7579b2fc635f
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2021-04-30 10:57:33 +0530

    Commit

--- Arrays/012.py
+++ Arrays/012.py
@@ -1,6 +1,11 @@
-def merge(arr1, arr2, n, m): 
-        temp = arr2[0]
-        for i in range(n):
-            if arr1[i]>arr2[0]:
-                arr1[i],arr2[0]=arr2[0],arr1[i]
-                arr2.sort()
+#Merge 2 sorted arrays without using Extra space.
+def merge(n1, n2, m, n):
+        i = m-1
+        j = 0
+        while i>=0 and j<n:
+            if n1[i]>n2[j]:
+                n1[i],n2[j]=n2[j],n1[i]
+            i-=1
+            j+=1
+        n1.sort()
+        n2.sort()
--- Arrays/013.py
+++ Arrays/013.py
@@ -1,11 +1,11 @@
-#Merge 2 sorted arrays without using Extra space.
-def merge(n1, n2, m, n):
-        i = m-1
-        j = 0
-        while i>=0 and j<n:
-            if n1[i]>n2[j]:
-                n1[i],n2[j]=n2[j],n1[i]
-            i-=1
-            j+=1
-        n1.sort()
-        n2.sort()
+#Kedane's Algo
+def maxSubArraySum(a):
+        temp=0
+        maxSum=0
+        for i in a:
+            temp+=i
+            if temp>maxSum:
+                maxSum=temp
+            if temp<0:
+                temp=0
+        return maxSum
